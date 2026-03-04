117ccd23400289c28d37ea35fd2ca7e5807f3614
r-on-y <ronyarif800@gmail.com>
2026-03-04 10:09:31 +0600

    Appointment model addition

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from patient.models import Patient
+from doctor.models import Doctor,AvailableTime
+
 
 # Create your models here.
+
+Appointment_Status=[
+    ('Completed','Completed'),
+    ('Pending','Pending'),
+    ('Running','Running')
+]
+
+Appointment_Types=[
+    ('Offline','Offline'),
+    ('Online','Online')
+]
+
+class Appoitnment(models.Model):
+    patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
+
+    appointment_type=models.CharField(choices=Appointment_Types,max_length=10)
+    appointment_status=models.CharField(choices=Appointment_Status,max_length=10,default="Pending")
+
+    symptom=models.TextField()
+
+    time=models.OneToOneField(AvailableTime,on_delete=models.CASCADE)
+
+    cancel=models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Doctor {self.doctor.user.first_name}, Patient {self.patient.user.first_name}"
+
+
+
+
